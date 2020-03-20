0c3d269c8327a7da96a80888940aaf1beab5ef45
MoisesL27 <leonormoises27@gmail.com>
2020-03-19 23:22:18 -0600

    Build project

--- src/models/Table.ts
+++ src/models/Table.ts
@@ -73,7 +73,7 @@ class Table {
 
   async deleteRows(rowIds: string[]): Promise<boolean> {
     // docs/{docId}/tables/{tableIdOrName}/rows
-    const params = {rowIds};
+    const params = {rowIds: rowIds};
     const { status } = await this.API.request(`/docs/${this.docId}/tables/${this.id}/rows`, params , 'DELETE');
     return status === 202;
   }
